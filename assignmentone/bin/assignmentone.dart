11460aa66cb01608df07fc4e934a20e45d9ee515
bakibillah93 <132567912+bakibillah93@users.noreply.github.com>
2023-08-03 18:40:11 +0600

    Add files via upload

---
+++ assignmentone/bin/assignmentone.dart
@@ -0,0 +1,20 @@
+void main(){
+  //Declare constant variables 'a' and 'b' with integer values
+  const int a = 50;
+  const int b = 10;
+
+  //Perform addition, subtraction, multiplication, division, and modulus
+  int addition = a + b;
+  int subtraction = a - b;
+  int multiplication = a * b;
+  double division = a / b;
+  int modulus = a % b;
+
+  // Print the results
+  print ('Additon of a and b : $addition');
+  print ('Subtraction of a and b : $subtraction');
+  print ('Multiplication of a and b : $multiplication');
+  print ('Division of a and b : $division');
+  print ('Modulus of a and b : $modulus');
+
+}
